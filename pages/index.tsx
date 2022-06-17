edc21b91fbda0e1ac9780ae84d20eaea3d0600b2
gauraklein <gaurakklein@gmail.com>
2022-06-16 23:00:25 -0500

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
 import Layout from "../components/layout";
-import Counter from "../components/counter";
 
 const Index = () => (
-  <Layout home>
+  <Layout >
     <section className="section">
       <div className="container">
         <h1 className="title">
-          Hello World from <a href="https://nextjs.org/">Next.js</a> and{" "}
-          <a href="https://bulma.io/">Bulma</a>!
+          Vee's Baking Calculator
         </h1>
-      </div>
-    </section>
-
-    <section className="section">
-      <div className="container">
-        <Counter></Counter>
       </div>
     </section>
   </Layout>
