d44ae1df60d5ddc5099b46fa013b4148d362e025
gauraklein <gaurakklein@gmail.com>
2022-06-16 23:16:34 -0500

    some color stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 const Index = () => (
   <Layout >
     <section className="section">
-      <div className="container">
+      <div className="container has-text-centered">
         <h1 className="title">
           Vee's Baking Calculator
         </h1>
